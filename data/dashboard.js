c3e26831992cfa9d806a007fe83bbecd262936b6
kabps3309 <kabps3309@gmail.com>
2026-06-23 16:04:44 +0700

    Create dashboard.js

---
+++ data/dashboard.js
@@ -0,0 +1,54 @@
+{
+  "updated_at": "2026-06-23 08:00 WIB",
+
+  "summary": {
+    "total_sls": 5000,
+    "selesai": 3500,
+    "belum": 1500,
+    "progress": 70
+  },
+
+  "kecamatan": [
+    {
+      "kode": "330901",
+      "nama": "Ampel",
+      "target": 500,
+      "selesai": 380
+    },
+    {
+      "kode": "330902",
+      "nama": "Simo",
+      "target": 450,
+      "selesai": 320
+    },
+    {
+      "kode": "330903",
+      "nama": "Andong",
+      "target": 600,
+      "selesai": 500
+    }
+  ],
+
+  "history": [
+    {
+      "tanggal": "19 Jun",
+      "nilai": 1500
+    },
+    {
+      "tanggal": "20 Jun",
+      "nilai": 2200
+    },
+    {
+      "tanggal": "21 Jun",
+      "nilai": 2800
+    },
+    {
+      "tanggal": "22 Jun",
+      "nilai": 3200
+    },
+    {
+      "tanggal": "23 Jun",
+      "nilai": 3500
+    }
+  ]
+}
